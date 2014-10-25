e255658598709beabd2dad57a8dd7e774ac617ec
Daniel Kesler <danielckesler@gmail.com>
2014-10-25 16:26:20 -0600

    initial upload

---
+++ fadeScroll.js
@@ -0,0 +1,25 @@
+var app = angular.module("blog");
+
+app.directive('fadeScroll', function(){
+    return{
+        restrict: 'AE',
+        scope: {
+            fade: '@fade',
+            start: '@start'
+        },
+        link: function (scope, elem, attrs){
+            var theFade = function(){
+                var startSpot = scope.start || 0;
+                var fadeFactor = scope.fade || 1;
+                var relativePoint = ($(window).scrollTop() - startSpot);
+                if( relativePoint >= 0){
+                    var factor = (( relativePoint/1000 ) * fadeFactor);
+                    console.log('factor: ', factor);
+                    elem.css('opacity', (1-factor));
+                }
+            };
+            $(window).scroll(theFade);
+        }
+
+    }
+});
